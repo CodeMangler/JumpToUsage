fb6cefd6ebf9ed8c433ef418c5d16371eb6b75c2
CodeMangler <hsdpal@gmail.com>
2006-10-18 19:20:19 +0000

    Fixed NPE when the action is invoked on non editor windows.. Ex, on the Project manager
    Action runs in a separate thread, so it doesn't freeze the UI if it takes long to finish..

--- src/pal/plugins/intellij/jumptousage/JumpToUsageAction.java
+++ src/pal/plugins/intellij/jumptousage/JumpToUsageAction.java
@@ -3,6 +3,7 @@ package pal.plugins.intellij.jumptousage;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataConstants;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNamedElement;
 import pal.plugins.intellij.jumptousage.finders.PsiElementUsageFinderFactory;
@@ -13,19 +14,31 @@ import pal.plugins.intellij.jumptousage.wrappers.DataHolder;
 import pal.plugins.intellij.jumptousage.wrappers.ReferenceCollection;
 
 public class JumpToUsageAction extends AnAction {
-    public void actionPerformed(AnActionEvent e) {
-        PsiElement psiElement = (PsiElement) e.getDataContext().getData(DataConstants.PSI_ELEMENT);
-        if (!(psiElement instanceof PsiNamedElement)) return;
 
+    public void update(AnActionEvent e) {
+        e.getPresentation().setEnabled(e.getDataContext().getData(DataConstants.EDITOR) != null);
+    }
+
+    public void actionPerformed(AnActionEvent e) {
         DataHolder.getInstance().initDataHolder(e.getDataContext());
 
-        ReferenceCollection references = PsiElementUsageFinderFactory.getUsageFinder(psiElement).findUsages();
-        if (references.size() == 1) {
-            new ReferenceNavigator(references.iterator().next()).navigateToReference();
-        } else {
-            PresentationLocation presentationLocation = new PresentationLocation(PresentationLocation.currentCaretLocation());
-            presentationLocation.translate(0, DataHolder.getInstance().EDITOR.getLineHeight());
-            new ReferencePresentation(references, psiElement).present(presentationLocation);
-        }
+        final PsiElement psiElement = DataHolder.getInstance().PSI_ELEMENT;
+        if (!(psiElement instanceof PsiNamedElement)) return;
+
+        if (DataHolder.getInstance().EDITOR == null) return;
+
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            public void run() {
+                ReferenceCollection references = PsiElementUsageFinderFactory.getUsageFinder(psiElement).findUsages();
+                if (references.size() == 1) {
+                    new ReferenceNavigator(references.iterator().next()).navigateToReference();
+                } else {
+                    PresentationLocation presentationLocation = new PresentationLocation(PresentationLocation.currentCaretLocation());
+                    presentationLocation.translate(0, DataHolder.getInstance().EDITOR.getLineHeight());
+                    new ReferencePresentation(references, psiElement).present(presentationLocation);
+                }
+
+            }
+        });
     }
 }
--- src/pal/plugins/intellij/jumptousage/wrappers/ReferenceCollection.java
+++ src/pal/plugins/intellij/jumptousage/wrappers/ReferenceCollection.java
@@ -1,14 +1,12 @@
 package pal.plugins.intellij.jumptousage.wrappers;
 
 import com.intellij.psi.PsiReference;
+import com.intellij.util.containers.ArrayListSet;
 
-import java.util.AbstractCollection;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 public class ReferenceCollection extends AbstractCollection<Reference> {
-    private ArrayList<Reference> references = new ArrayList<Reference>();
+    private Set<Reference> references = new ArrayListSet<Reference>();
     public static ReferenceCollection EMPTY = new ReferenceCollection(new ArrayList<PsiReference>());
 
     public ReferenceCollection(Collection<PsiReference> psiReferences) {
@@ -25,22 +23,8 @@ public class ReferenceCollection extends AbstractCollection<Reference> {
         return references.iterator();
     }
 
-    public boolean addAll(Collection<? extends Reference> references) {
-        return this.references.addAll(references);
-    }
-
-
     public boolean add(Reference reference) {
         return references.add(reference);
-    }
-
-    public ReferenceCollection removeDuplicates() {
-        ReferenceCollection uniqueReferences = ReferenceCollection.EMPTY;
-        for (Reference reference : references) {
-            if (!uniqueReferences.contains(reference))
-                uniqueReferences.add(reference);
-        }
-        return uniqueReferences;
     }
 
     public int size() {
